dd29cca617ccb9fea1bcb710989eb00243d8afff
karri <karri@sipo.fi>
2024-01-09 19:56:42 +0200

    streamline

--- build26rom4k.py
+++ build26rom4k.py
@@ -47,110 +47,27 @@ int main() {
     set_sys_clock_pll(1600000000, 4, 1);
     
     // GPIO setup.
-    setup_gpio();
+    gpio_init_mask(0x7fffff);          // All pins.
+    gpio_set_dir_in_masked(0x7fff);    // Address pins.
+    gpio_set_dir_out_masked(0x7f8000); // Data pins.
     
     // Continually check address lines and
     // put associated data on bus.
     while (true) {
         // Set the data on the bus anyway
         addr = gpio_get_all();
-        put_data_on_bus(addr);
-
+        gpio_put_masked(0x7f8000, rom_contents[addr & ROM_MASK] << 15);
         // Disable data bus output if it was a ROM access
 	new_rom_in_use = (addr & ROM_IN_USE) ? 1 : 0;
         if (new_rom_in_use != rom_in_use) {
             rom_in_use = 1 - rom_in_use;
             if (rom_in_use) {
-                data_bus_output();
+                gpio_set_dir_out_masked(0x7f8000);
             } else {
-                data_bus_input();
+                gpio_set_dir_in_masked(0x7f8000);
             }
         }
     }
-}
-
-void setup_gpio() {
-    // Address pins.
-    gpio_init(A0);
-    gpio_set_dir(A0, GPIO_IN);
-    gpio_init(A1);
-    gpio_set_dir(A1, GPIO_IN);
-    gpio_init(A2);
-    gpio_set_dir(A2, GPIO_IN);
-    gpio_init(A3);
-    gpio_set_dir(A3, GPIO_IN);
-    gpio_init(A4);
-    gpio_set_dir(A4, GPIO_IN);
-    gpio_init(A5);
-    gpio_set_dir(A5, GPIO_IN);
-    gpio_init(A6);
-    gpio_set_dir(A6, GPIO_IN);
-    gpio_init(A7);
-    gpio_set_dir(A7, GPIO_IN);
-    gpio_init(A8);
-    gpio_set_dir(A8, GPIO_IN);
-    gpio_init(A9);
-    gpio_set_dir(A9, GPIO_IN);
-    gpio_init(A10);
-    gpio_set_dir(A10, GPIO_IN);
-    gpio_init(A11);
-    gpio_set_dir(A11, GPIO_IN);
-    gpio_init(A12);
-    gpio_set_dir(A12, GPIO_IN);
-    gpio_init(A13);
-    gpio_set_dir(A13, GPIO_IN);
-    gpio_init(A14);
-    gpio_set_dir(A14, GPIO_IN);
-
-    // Data pins.
-    gpio_init(D0);
-    gpio_set_dir(D0, GPIO_OUT);
-    gpio_init(D1);
-    gpio_set_dir(D1, GPIO_OUT);
-    gpio_init(D2);
-    gpio_set_dir(D2, GPIO_OUT);
-    gpio_init(D3);
-    gpio_set_dir(D3, GPIO_OUT);
-    gpio_init(D4);
-    gpio_set_dir(D4, GPIO_OUT);
-    gpio_init(D5);
-    gpio_set_dir(D5, GPIO_OUT);
-    gpio_init(D6);
-    gpio_set_dir(D6, GPIO_OUT);
-    gpio_init(D7);
-    gpio_set_dir(D7, GPIO_OUT);
-}
-
-void data_bus_input() {
-    // Data pins.
-    gpio_set_dir(D0, GPIO_IN);
-    gpio_set_dir(D1, GPIO_IN);
-    gpio_set_dir(D2, GPIO_IN);
-    gpio_set_dir(D3, GPIO_IN);
-    gpio_set_dir(D4, GPIO_IN);
-    gpio_set_dir(D5, GPIO_IN);
-    gpio_set_dir(D6, GPIO_IN);
-    gpio_set_dir(D7, GPIO_IN);
-}
-
-void data_bus_output() {
-    // Data pins.
-    gpio_set_dir(D0, GPIO_OUT);
-    gpio_set_dir(D1, GPIO_OUT);
-    gpio_set_dir(D2, GPIO_OUT);
-    gpio_set_dir(D3, GPIO_OUT);
-    gpio_set_dir(D4, GPIO_OUT);
-    gpio_set_dir(D5, GPIO_OUT);
-    gpio_set_dir(D6, GPIO_OUT);
-    gpio_set_dir(D7, GPIO_OUT);
-}
-
-void put_data_on_bus(int address) {
-    // int data = rom_contents[address];
-
-    // gpio mask = 8355840; // i.e.: 11111111000000000000000
-    // Shift data 15 bits to put it in correct position to match data pin defintion.
-    gpio_put_masked(8355840, rom_contents[address & ROM_MASK] << 15);
 }
 
 void setup_rom_contents() {
